1015ccf7e90fc57e3812d6f6ef468dda43034f43
vanssx3 <144460118+vanssx3@users.noreply.github.com>
2023-12-20 11:10:25 -0600

    Update yorkCrack.py

--- yorkCrack.py
+++ yorkCrack.py
@@ -2,58 +2,36 @@ import pyautogui
 import string
 import itertools
 
-allChars = (string.ascii_letters + string.digits + string.punctuation)
 passlist = './vampork.txt'
-
+symbols = '!?'
 print("You are being cracked B)")
 
 def generateStuff():
-    for length in range(3, 4):
-        for combination in itertools.product(allChars, repeat=length):
-            guess = "".join(combination)
-            guess = guess + "1997"
-            with open("vampork.txt", 'a') as york:
-                york.write(guess)
-                york.write("\n")
-
-
     for length in range(2, 3):
-        for combination in itertools.product(allChars, repeat=length):
+        for combination in itertools.product(string.ascii_letters, repeat=length):
             guess = "".join(combination)
-            guess = guess + "1997"
             for length2 in range(1, 2):
-                for combination2 in itertools.product(allChars, repeat=length2):
+                for combination2 in itertools.product(symbols):
                     guess2 = guess + "".join(combination2)
-                    with open("vampork.txt", 'a') as york:
-                        york.write(guess2)
+                    finalGuess = guess2 + "1997"
+                    with open("vampork.txt", "a") as york:
+                        york.write(finalGuess)
                         york.write("\n")
-    for length in range(1, 2):
-        for combination in itertools.product(allChars, repeat=length):
-            guess = "".join(combination)
-            guess = guess + "1997"
-            for length2 in range(2, 3):
-                for combination2 in itertools.product(allChars, repeat=length2):
-                    guess2 = guess + "".join(combination2)
-                    with open("vampork.txt", 'a') as york:
-                        york.write(guess2)
-                        york.write("\n")
-    
-    for length in range(3, 4):
-        for combination in itertools.product(allChars, repeat=length):
-            guess = "".join(combination)
-            guess = "1997" + guess
-            with open("vampork.txt", 'a') as york:
-                york.write(guess)
-                york.write("\n")
-                print("Passwords generated, time for attackalacking")
 
+
+    
+    print("Passwords generated, time for attackalacking")
+    attackalack()
 
 def attackalack():
-    print("ok crandle add your stuff")
+    print("do the thing crandall!")
 
 
 
-    # do this command when everything is done
+    # run this when youre done
     raise SystemExit
 
+
 generateStuff()
+
+
